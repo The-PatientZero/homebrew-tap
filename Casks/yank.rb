e48f30b434a22a84aa0aff5ca4e12e19f16b9271
The-PatientZero <The-PatientZero@users.noreply.github.com>
2026-06-21 20:34:40 +0000

    yank 1.0.0

--- Casks/yank.rb
+++ Casks/yank.rb
@@ -1,6 +1,6 @@
 cask "yank" do
   version "1.0.0"
-  sha256 "REPLACE_WITH_DMG_SHA256"
+  sha256 "beadc2dfef5ea8bd4ceacea5e565a65b51cd50d5f45148816b4d0f5eda5572ee"
 
   url "https://github.com/The-PatientZero/yank/releases/download/v#{version}/Yank.dmg",
       verified: "github.com/The-PatientZero/yank/"
